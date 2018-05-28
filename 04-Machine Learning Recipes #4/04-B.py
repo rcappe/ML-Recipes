0024c95ee342f610612a2a731574e4b37aed969e
Roberto Cappelletti <roberto.cappelletti@elfo.net>
2018-05-28 17:23:27 +0200

    Machine Learning Recipes #4

---
+++ 04-Machine Learning Recipes #4/04-B.py
@@ -0,0 +1,24 @@
+#https://www.youtube.com/watch?v=84gqSbLcBFE&list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2ryal&index=4
+
+# import a dataset
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+X = iris['data']
+y = iris['target']
+
+from sklearn.model_selection import train_test_split #sklearn.cross_validation is deprecaded
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.5)
+
+# http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+from sklearn.neighbors import KNeighborsClassifier
+my_classifier = KNeighborsClassifier()
+
+my_classifier.fit(X_train, y_train)
+
+predictions = my_classifier.predict(X_test)
+print (predictions)
+
+from sklearn.metrics import accuracy_score
+print (accuracy_score(y_test,predictions))
