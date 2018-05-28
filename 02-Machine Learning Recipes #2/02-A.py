8cd1c7a26b3b55ac6d0461b891aea1afeb376601
Roberto Cappelletti <roberto.cappelletti@elfo.net>
2018-05-28 16:56:29 +0200

    fix pylint warning - instance of tuple has no data member

--- 02-Machine Learning Recipes #2/02-A.py
+++ 02-Machine Learning Recipes #2/02-A.py
@@ -5,10 +5,10 @@
 
 from sklearn.datasets import load_iris
 iris = load_iris()
-print (iris.feature_names)
-print (iris.target_names)
-print (iris.data[0])
+print (iris['feature_names'])
+print (iris['target_names'])
+print (iris['data'][0])
 
-for i in range(len(iris.target)):
-    print ("Exemple %d: label %s, features %s" % (i, iris.target[i], iris.data[i]))
+for i in range(len(iris['target'])):
+    print ("Exemple %d: label %s, features %s" % (i, iris['target'][i], iris['data'][i]))
 
