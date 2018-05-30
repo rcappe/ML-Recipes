120c4fd96a66c8dd9606ce1ed1ce5d007526c7ec
Roberto Cappelletti <roberto.cappelletti@elfo.net>
2018-05-30 12:07:57 +0200

    Machine Learning Recipes #7

---
+++ 07-Machine Learning Recipes #7/07-A.py
@@ -0,0 +1,47 @@
+#https://www.youtube.com/watch?v=Gj0iyo265bc&index=7&list=PLOU2XLYxmsIIuiBfYad6rFYQU_jL2ryal
+
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+learn = tf.contrib.learn
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+## Import dataset
+
+mnist = learn.datasets.load_dataset('mnist')
+data = mnist.train.images
+labels = np.asarray(mnist.train.labels, dtype=np.int32)
+test_data = mnist.test.images
+test_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+## Display some digit
+
+def display(i):
+    img = test_data[i]
+    plt.title('Example %d. Label: %d' % (i, test_labels[i]))
+    plt.imshow(img.reshape((28,28)), cmap=plt.cm.gray_r)
+    plt.show()
+
+#display(0)
+
+max_examples = 10000
+data = data[:max_examples]
+labels = labels[:max_examples]
+
+## Fit a Linear Classifier
+
+feature_columns = learn.infer_real_valued_columns_from_input(data)
+classifier = learn.LinearClassifier(feature_columns=feature_columns, n_classes=10)
+classifier.fit(data, labels, batch_size=100, steps=1000)
+
+## Evaluate accuracy
+
+classifier.evaluate(test_data, test_labels)
+print (classifier.evaluate(test_data, test_labels)["accuracy"])
+
+## Classify a few examples
+
+prediction = classifier.predict(np.array([test_data[0]], dtype=float), as_iterable=False)
+print("Predicted %d, Label: %d" % (prediction, test_labels[0]))
+
+display(0)
